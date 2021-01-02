60ae8de6924b93427960af9ed3f09361134ff510
Konstantinos <kwstas654321@gmail.com>
2021-01-02 11:54:33 +0200

    Password

--- Project/Loothub/src/main/java/com/company/springboot/configuration/SecurityConfiguration.java
+++ Project/Loothub/src/main/java/com/company/springboot/configuration/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/loginsignup?logout")
+                .logoutSuccessUrl("/loginsignup?logout")                
                 .permitAll();
                  
     }
