0ed4ab9b626bd6376671dc1ce9b5660ef73e3c66
Konstantinos <kwstas654321@gmail.com>
2021-01-12 16:03:27 +0200

    Proper Archtecture

--- Project/Loothub/src/main/java/com/company/springboot/controllers/UserRegistrationController.java
+++ Project/Loothub/src/main/java/com/company/springboot/controllers/UserRegistrationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
-
+    
     private UserService userService;
 
     public UserRegistrationController(UserService userService) {
