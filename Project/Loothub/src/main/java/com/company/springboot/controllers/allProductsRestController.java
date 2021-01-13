0d53a84ce703d188882d4535eb1528e4dcd327b0
Konstantinos <kwstas654321@gmail.com>
2021-01-14 01:07:45 +0200

    Almost Done

--- Project/Loothub/src/main/java/com/company/springboot/controllers/allProductsRestController.java
+++ Project/Loothub/src/main/java/com/company/springboot/controllers/allProductsRestController.java
@@ -3,6 +3,7 @@ package com.company.springboot.controllers;
 import com.company.springboot.entities.Product;
 import com.company.springboot.entities.dto.ProductDto;
 import com.company.springboot.services.ProductService;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,19 +20,31 @@ public class allProductsRestController {
 
     @CrossOrigin("http://localhost:8080")
     @GetMapping("/productList")
-    public ProductDto get() {
+    public List<ProductDto> get() {
         List<Product> products = productService.listAll();
+        
+        List<ProductDto> listproductsDto = new ArrayList<>();
         ProductDto productDto = new ProductDto();
 
         Product product1 = new Product();
+        int i = 0;
         for (Product product : products) {
+            
             product1 = product;
+            listproductsDto.add(new ProductDto(
+                    product1.getName(),
+                    product1.getPrice(),
+                    product1.getCategory(),
+                    product1.getDescription()
+                    ));
         }
-        productDto.setCategory(product1.getCategory());
-        productDto.setDescription(product1.getDescription());
-        productDto.setName(product1.getName());
-        productDto.setPrice(product1.getPrice());
-        return productDto;
+          System.out.println(products);
+//        productDto.setCategory(product1.getCategory());
+//        productDto.setDescription(product1.getDescription());
+//        productDto.setName(product1.getName());
+//        productDto.setPrice(product1.getPrice());
+
+        return listproductsDto;
     }
 
 }
--- Project/Loothub/src/main/resources/static/js/productsRest.js
+++ Project/Loothub/src/main/resources/static/js/productsRest.js
@@ -1,11 +1,41 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/api/productList"
-    }).then(function(data) {
-       
-       $('#category').append(data.category);
-       $('#description').append(data.description);
-       $('#id').append(data.id);
-       $('#price').append(data.price);
-    });
+$(document).ready(function () {
+//    $.ajax({
+//        url: "http://localhost:8080/api/productList"
+//    }).then(function (data) {
+//         
+//
+//        $('#category').append(data.category);
+//        $('#description').append(data.description);
+//        $('#name').append(data.name);
+//        $('#id').append(data.id);
+//        $('#price').append(data.price);
+//    });
+    
+    $.ajax({ 
+    type: 'GET', 
+    url: "http://localhost:8080/api/productList",
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: function (data) { 
+        $.each(data, function(index, element) {
+            $('body').append($('<h1>', {
+                text: "Product{ Category" + element.category + 
+                        ", Name"+element.name+
+                        ", Price"+element.price+
+                        ", Description"+element.description+
+                        "}"
+                
+            }));
+            $('body').append($('<h2>', {
+                text: "Product{ Category" + element.category + 
+                        ", Name"+element.name+
+                        ", Price"+element.price+
+                        ", Description"+element.description+
+                        "}"
+                
+            }));
+            
+        });
+    }
+});
 });
