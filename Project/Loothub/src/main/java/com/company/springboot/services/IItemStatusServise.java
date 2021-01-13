f3b703dc6cf714b4578753aed589007c5a618fdd
triantadim <d.trianta3@gmail.com>
2021-01-13 09:28:19 +0200

    creation of ItemStatus CRUD
    creation of ItemStatus CRUD

---
+++ Project/Loothub/src/main/java/com/company/springboot/services/IItemStatusServise.java
@@ -0,0 +1,14 @@
+package com.company.springboot.services;
+
+import com.company.springboot.entities.ItemStatus;
+import java.util.List;
+
+
+public interface IItemStatusServise {
+    public List<ItemStatus> listAll();
+    public ItemStatus save(ItemStatus itemStatus);
+    public ItemStatus get(Long id);
+    public void delete(Long id);
+    public void update(ItemStatus itemStatus);
+    
+}
