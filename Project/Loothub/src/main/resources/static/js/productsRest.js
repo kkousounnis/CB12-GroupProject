0c5bd0648bac00f55711897aee98f4a797be3d6e
Konstantinos <kwstas654321@gmail.com>
2021-01-15 21:01:41 +0200

    Cards Almost Completed

--- Project/Loothub/src/main/resources/static/js/productsRest.js
+++ Project/Loothub/src/main/resources/static/js/productsRest.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 //        $('#id').append(data.id);
 //        $('#price').append(data.price);
 //    });
-    
+    var x = 1;
     $.ajax({ 
     type: 'GET', 
     url: "http://localhost:8080/api/productList",
@@ -19,25 +19,45 @@ $(document).ready(function () {
     success: function (data) { 
         $.each(data, function(index, element) {
             var k = element.imagePath;
-            $('main').append($('<h1>', {
+            $('main').append($('<h5>', {
                 text: "Product{ Category" + element.category + 
+                        ", Id"+element.id+
                         ", Name"+element.name+
                         ", Price"+element.price+
                         ", Description"+element.description+
-                        "Image path"+element.imagePath+
-                        "Hello "+
-                        "FileName "+element.fileName+
+                        ", Category"+element.category+
+                        ", Product Id"+element.productImageId+
+                        ", Image path"+element.imagePath+
+                        ", FileName "+element.fileName+
+                        ", User Id"+ element.userId+
                         "}"
                 
             }));
-            $('main').append($('<h2>', {
-                text: "Product{ Category" + element.category + 
-                        ", Name"+element.name+
-                        ", Price"+element.price+
-                        ", Description"+element.description+
-                        "}"
+            let newElement =  $(
+                    ''+
+                    '<div class="card p-3 mb-5 rounded" style="width: 18rem;">'+ 
+                    '<img src="/img/products/1-leo_francis.jpeg" class="card-img-top" alt="...">'+
+                    '<div class="card-body pull-left text-black">'+
+                    '<h5 class="card-title">Card title</h5>'+
+                    '<p class="card-text">Some quick example '+
+                    'text to build on the card title and make'+
+                    ' up the bulk of the cards content.</p>'+            
+                    ' up the bulk of the cards content.</p>'+            
+                    ' </div>'+            
+                    +''
+                    );
+            $('.row.card-deck').append($(newElement, {
                 
-            }));
+            }));  
+            
+//            newElement.appendTo("#container"+x);
+//            if(x<=3){
+//                x++;
+//            }else{
+//                
+//                x=1;
+//            }
+            console.log(x);
             $('main').append($('<img src="/img/products/'+element.fileName+'">', {
                 text: " Product{ Category" + element.category + 
                         ", Name"+element.name+
