1e506586639d5cda0337c9cac47a4f85b6ce26ee
Konstantinos <kwstas654321@gmail.com>
2021-01-17 19:44:05 +0200

    Open Modal Product

--- Project/Loothub/src/main/resources/static/js/productsRest.js
+++ Project/Loothub/src/main/resources/static/js/productsRest.js
@@ -11,46 +11,60 @@ $(document).ready(function () {
 //        $('#price').append(data.price);
 //    });
     var x = 1;
-    $.ajax({
-        type: 'GET',
-        url: "http://localhost:8080/api/productList",
-        data: {get_param: 'value'},
-        dataType: 'json',
-        success: function (data) {
-            $.each(data, function (index, element) {
-                let fileName = element.fileName;
+    $.ajax({ 
+    type: 'GET', 
+    url: "http://localhost:8080/api/productList",
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: function (data) { 
+        $.each(data, function(index, element) {
+            let fileName = element.fileName;
+            
+//            $('main').append($('<h5>', {
+//                text: "Product{ Category" + element.category + 
+//                        ", Id"+element.id+
+//                        ", Name"+element.name+
+//                        ", Price"+element.price+
+//                        ", Description"+element.description+
+//                        ", Category"+element.category+
+//                        ", Product Id"+element.productImageId+
+//                        ", Image path"+element.imagePath+
+//                        ", FileName "+element.fileName+
+//                        ", User Id"+ element.userId+
+//                        "}"
+                
+           // }));
+            let newElement =  $(
+                    ''+
+                    '<div class="col-md-4">'+
+                    '<figure class="card card-product-grid card-lg " >'+ 
+                    '<a href="#" class="img-wrap mx-auto d-block" data-abc="true">'+                                
+                    '<img style="width: 200px;height: 200px" src="/img/products/'+fileName+'">'+                                
+                    '</a>'+                                
+                    '<figcaption class="info-wrap">'+                                
+                    '<div class="row">'+                                
+                    '<div class="col-md-8">'+                                
+                    '<a href="#" class="title" data-abc="true" data-toggle="modal" data-target="#exampleModal" data-whatever="/img/products/'+fileName+'" data-whatever="'+element.name+'"  value="'+element.name+'" >'+element.name+'</a>'+                    
+                    '</div>'+                                
+                    '<div class="col-md-4">'+                                
+                    '<div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>'+                                
+                    '</div>'+                                
+                    '</div>'+                                
+                    '</figcaption>'+                                
+                    '<div class="bottom-wrap"> <a href="/loginsignup#SigninProduct?'+element.name+'"  type="submit"  class="btn btn-primary float-right" data-abc="true" value="Buy now">Buy now </a>'+                                
+                    '<div class="price-wrap"> <span class="price h5" value="'+element.price+'">'+element.price+'&euro;</span> <br> <small class="text-success">Free shipping</small> </div>'+                                
+                    '</div>'+                                
+                    '</figure>'+                                
+                    '</div>'+ 
+                    +''
+                    );
+            $('.row.card-deck').append($(newElement, {
+                
+            }));  
+            
 
-                let newElement = $(
-                        '' +
-                        '<div class="col-md-4">' +
-                        '<figure class="card card-product-grid card-lg " >' +
-                        '<a href="#" class="img-wrap mx-auto d-block" data-abc="true">' +
-                        '<img style="width: 200px;height: 200px" src="/img/products/' + fileName + '">' +
-                        '</a>' +
-                        '<figcaption class="info-wrap">' +
-                        '<div class="row">' +
-                        '<div class="col-md-8">' +
-                        '<a href="#" class="title" data-abc="true" value="' + element.name + '">' + element.name + '</a>' +
-                        '</div>' +
-                        '<div class="col-md-4">' +
-                        '<div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</figcaption>' +
-                        '<div class="bottom-wrap"> <a href="/loginsignup#SigninProduct"  type="submit"  class="btn btn-primary float-right" data-abc="true" value="Buy now">Buy now </a>' +
-                        '<div class="price-wrap"> <span class="price h5" value="' + element.price + '">' + element.price + '&euro;</span> <br> <small class="text-success">Free shipping</small> </div>' +
-                        '</div>' +
-                        '</figure>' +
-                        '</div>' +
-                        +''
-                        );
-                $('.row.card-deck').append($(newElement, {
-
-                }));
-
-
-
-            });
-        }
-    });
+            
+        });
+    }
+});
 });
