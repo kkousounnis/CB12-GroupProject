89f0036c9240df44d7e6829a1ddfbd27f12f6c1e
Dimitris Kasimatis <dimkasimatis92@gmail.com>
2021-01-24 16:14:59 +0200

    profile controller changes

--- Project/Loothub/src/main/java/com/company/springboot/controllers/ProfileController.java
+++ Project/Loothub/src/main/java/com/company/springboot/controllers/ProfileController.java
@@ -2,16 +2,16 @@ package com.company.springboot.controllers;
 
 import com.company.springboot.entities.ContactNumber;
 import com.company.springboot.entities.UserAddress;
-import com.company.springboot.entities.dto.AddressDto;
-import com.company.springboot.entities.dto.ContactNumberDto;
-import com.company.springboot.entities.dto.UserDto;
-import com.company.springboot.entities.dto.UserProfileDto;
+import com.company.springboot.entities.dto.OrderDto;
+import com.company.springboot.services.UserAddressService;
 import com.company.springboot.services.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,32 +19,37 @@ public class ProfileController {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UserAddressService userAddressService;
 
     @GetMapping("/profile")
     public ModelAndView profileView(@CurrentSecurityContext(expression = "authentication?.name") String username) {
 
-        
-        UserProfileDto profileDto = new UserProfileDto();
-        
-        profileDto.setUserDto(new UserDto(userService.findByEmailAddress(username).getFirstName(), 
-                userService.findByEmailAddress(username).getLastName(),
-                userService.findByEmailAddress(username).getEmail()));
-        
-        profileDto.setContactDto(new ContactNumberDto((List<ContactNumber>) userService.findByEmailAddress(username).getContactNumbers()));
-        
-        profileDto.setAddressDto(new AddressDto((List<UserAddress>) userService.findByEmailAddress(username).getUserAddressList()));
-        
-        System.out.println(profileDto.getContactDto().getTelNumber().get(0) + "***HELLOOOOO");
-        
-        
-//        OrderDto orderDto = new OrderDto();
-//        orderDto.setFirstName(userService.findByEmailAddress(username).getFirstName());
-//        orderDto.setLastName(userService.findByEmailAddress(username).getLastName());
-//        orderDto.setEmail(userService.findByEmailAddress(username).getEmail());
-//        orderDto.setTelNumber((List<ContactNumber>) userService.findByEmailAddress(username).getContactNumbers());
+     
+        OrderDto orderDto = new OrderDto();
+        orderDto.setFirstName(userService.findByEmailAddress(username).getFirstName());
+        orderDto.setLastName(userService.findByEmailAddress(username).getLastName());
+        orderDto.setEmail(userService.findByEmailAddress(username).getEmail());
+        orderDto.setTelNumber((List<ContactNumber>) userService.findByEmailAddress(username).getContactNumbers());
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("profileDto", profileDto);
+        modelAndView.addObject("orderDto", orderDto);
         modelAndView.setViewName("profile");
+        return modelAndView;
+
+    }
+
+    @PostMapping("/edituser")
+    public ModelAndView editUser(@ModelAttribute("orderDto") OrderDto orderDto,
+            @CurrentSecurityContext(expression = "authentication?.name") String username) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        UserAddress userAddress = new UserAddress();
+        userAddress.setCountry(orderDto.getCountry());
+        userAddressService.save(userAddress);
+        
+        
+        
         return modelAndView;
     }
 
