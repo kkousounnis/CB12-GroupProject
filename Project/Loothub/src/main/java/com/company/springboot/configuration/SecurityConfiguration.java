ebe0004a1e14ca4bc70a0875a4cc56225d1b6119
triantadim <d.trianta3@gmail.com>
2021-01-24 21:27:44 +0200

    profile

--- Project/Loothub/src/main/java/com/company/springboot/configuration/SecurityConfiguration.java
+++ Project/Loothub/src/main/java/com/company/springboot/configuration/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                 "/registration**",              
-                "/buy**",              
+                "/buy**",  
                 "/error**",              
                 "/order**",              
                 "/{id}/**",              
