23ee038c7fde956615b99b63411d7e8e0e6c62fd
Konstantinos <kwstas654321@gmail.com>
2021-01-28 23:41:42 +0200

    Convert to War file

--- Project/Loothub/src/main/java/com/company/springboot/RegistrationLoginAthaurizationSpringBootApplication.java
+++ Project/Loothub/src/main/java/com/company/springboot/RegistrationLoginAthaurizationSpringBootApplication.java
@@ -2,12 +2,21 @@ package com.company.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RegistrationLoginAthaurizationSpringBootApplication {
+public class RegistrationLoginAthaurizationSpringBootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistrationLoginAthaurizationSpringBootApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(RegistrationLoginAthaurizationSpringBootApplication.class);
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationLoginAthaurizationSpringBootApplication.class, args);
+    }
 
 }
